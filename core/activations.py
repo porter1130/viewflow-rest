b79c53258eda40057db4f4cb0e7e8f2650e9f8f4
Porter <wjc1029@live.cn>
2018-11-26 08:44:41 +0800

    add Approval node

--- core/activations.py
+++ core/activations.py
@@ -2,81 +2,70 @@ from django.utils.timezone import now
 from viewflow import signals
 from viewflow.activation import Activation, STATUS, all_leading_canceled
 from viewflow.exceptions import FlowRuntimeError
+from viewflow.token import Token
 
 
 class ApprovalActivation(Activation):
 
     def __init__(self, **kwargs):
         self.next_task = None
+        self.tasks = []
+        self._split_count = None
         super(ApprovalActivation, self).__init__(**kwargs)
 
-    @Activation.status.transition(source=STATUS.NEW, target=STATUS.STARTED)
-    def start(self):
-        """Create Join task on the first incoming node complete."""
-        self.task.save()
-        signals.task_started.send(sender=self.flow_class, process=self.process, task=self.task)
+    def calculate_next(self):
+        self._split_count = self.flow_task.task_count_callback(self.process)
 
-    @Activation.status.transition(source=STATUS.STARTED)
-    def done(self):
-        """Complete the join within current exception propagation strategy."""
+    def activate_next(self):
+        pass
+
+    @Activation.status.transition(source=STATUS.NEW)
+    def assign_tasks(self):
         with self.exception_guard():
+            self.task.started = now()
+            self.task.save()
+
+            self.calculate_next()
+
             self.task.finished = now()
             self.set_status(STATUS.DONE)
             self.task.save()
 
-            signals.task_finished.send(sender=self.flow_class, process=self.process, task=self.task)
+            if self._split_count:
+                token_source = Token.split_token_source(self.task.token, self.task.pk)
+                for _ in range(self._split_count):
+                    task = self.flow_class.task_class(
+                        process=self.process,
+                        flow_task=self.flow_task,
+                        token=next(token_source),
+                        previous=self.task.previous
+                    )
+                    task.save()
+                    self.tasks.append(task)
+            elif self.flow_task._ifnone_next_node is not None:
+                self.flow_task._ifnone_next_node.activate(prev_activation=self, token=self.task.token)
+            else:
+                raise FlowRuntimeError(
+                    "{} activated with zero and no IfNone nodes specified".format(self.flow_task.name))
 
-            self.activate_next()
+    @classmethod
+    def activate(cls, flow_task, prev_activation, token):
+        flow_class, flow_task = flow_task.flow_class, flow_task
+        process = prev_activation.process
 
-    def is_done(self):
-        """Check that process can be continued futher.
+        task = flow_class.task_class(
+            process=process,
+            flow_task=flow_task,
+            token=token
+        )
 
-        Join check the all task state in db with the common token prefix.
+        task.save()
+        task.previous.add(prev_activation.task)
 
-        Join node would continue execution if all incoming tasks are DONE or CANCELED.
-        """
-        if not self.flow_task._wait_all:
-            return True
+        activation = cls()
+        activation.initialize(flow_task, task)
+        activation.assign_tasks()
 
-        join_prefixes = set(
-            prev.token.get_common_split_prefix(self.task.token, prev.pk)
-            for prev in self.task.previous.exclude(status=STATUS.CANCELED).all())
+        activation.calculate_next()
 
-        if len(join_prefixes) > 1:
-            raise FlowRuntimeError('Multiple tokens {} came to join {}'.format(join_prefixes, self.flow_task.name))
-
-        join_token_prefix = next(iter(join_prefixes))
-
-        active = self.flow_class.task_class._default_manager \
-            .filter(process=self.process, token__startswith=join_token_prefix) \
-            .exclude(status__in=[STATUS.DONE, STATUS.CANCELED])
-
-        return not active.exists()
-
-    @Activation.status.transition(source=STATUS.ERROR)
-    def retry(self):
-        """Manual join gateway reactivation after an error."""
-        if self.is_done():
-            self.done.original()
-
-    @Activation.status.transition(
-        source=[STATUS.ERROR, STATUS.DONE],
-        target=STATUS.STARTED,
-        conditions=[all_leading_canceled])
-    def undo(self):
-        """Undo the task."""
-        super(ApprovalActivation, self).undo.original()
-
-    @Activation.status.transition(source=[STATUS.NEW, STATUS.STARTED])
-    def perform(self):
-        """Manual gateway activation."""
-        if self.is_done():
-            self.done.original()
-
-    @Activation.status.transition(source=[STATUS.NEW, STATUS.STARTED], target=STATUS.CANCELED)
-    def cancel(self):
-        """Cancel existing join."""
-        super(ApprovalActivation, self).cancel.original()
-
-    def activate_next(self):
-        pass
+        return activation
--- core/nodes.py
+++ core/nodes.py
@@ -6,7 +6,7 @@ from core.activations import ApprovalActivation
 class Approval(mixins.TaskDescriptionMixin,
                mixins.NextNodeMixin,
                Gateway):
-    task_type = 'SPLIT'
+    task_type = 'HUMAN'
     activation_class = ApprovalActivation
 
     def __init__(self, wait_all=True, **kwargs):
