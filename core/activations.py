2d69f1f275332360e68884cbf1baf950549e5b6d
王敬超 <jcwang@ctrip.com>
2018-11-27 19:05:55 +0800

    update ApprovalActivation logical

--- core/activations.py
+++ core/activations.py
@@ -41,7 +41,6 @@ class ApprovalActivation(Activation):
                         previous=self.task.previous
                     )
                     task.save()
-                    self.tasks.append(task)
             elif self.flow_task._ifnone_next_node is not None:
                 self.flow_task._ifnone_next_node.activate(prev_activation=self, token=self.task.token)
             else:
@@ -66,6 +65,49 @@ class ApprovalActivation(Activation):
         activation.initialize(flow_task, task)
         activation.assign_tasks()
 
-        activation.calculate_next()
-
         return activation
+
+    def is_done(self):
+        """Check that process can be continued futher.
+
+        Join check the all task state in db with the common token prefix.
+
+        Join node would continue execution if all incoming tasks are DONE or CANCELED.
+        """
+        if not self.flow_task._wait_all:
+            return True
+
+        join_prefixes = set(
+            prev.token.get_common_split_prefix(self.task.token, prev.pk)
+            for prev in self.task.previous.exclude(status=STATUS.CANCELED).all())
+
+        if len(join_prefixes) > 1:
+            raise FlowRuntimeError('Multiple tokens {} came to join {}'.format(join_prefixes, self.flow_task.name))
+
+        join_token_prefix = next(iter(join_prefixes))
+
+        active = self.flow_class.task_class._default_manager \
+            .filter(process=self.process, token__startswith=join_token_prefix) \
+            .exclude(status__in=[STATUS.DONE, STATUS.CANCELED])
+
+        return not active.exists()
+
+    @Activation.status.transition(source=STATUS.STARTED)
+    def done(self):
+        """Complete the join within current exception propagation strategy."""
+        with self.exception_guard():
+            self.task.finished = now()
+            self.set_status(STATUS.DONE)
+            self.task.save()
+
+            #todo:cancel other tasks
+
+            signals.task_finished.send(sender=self.flow_class, process=self.process, task=self.task)
+
+            self.activate_next()
+
+    @Activation.status.transition(source=[STATUS.NEW, STATUS.STARTED])
+    def perform(self):
+        """Manual gateway activation."""
+        if self.is_done():
+            self.done.original()
